f01ef0a7d8b645b3ad17af6bcc8db00d6fa3d737
adriano <adrianobinan@gmail.com>
2024-12-03 23:04:42 -0300

    first commit

---
+++ frmCadastrarAssociado.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Sistema_de_Emprestimo___Biblioteca
+{
+    partial class frmPageAssociado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPageAssociado));
+            panelAssociado = new Panel();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panelAssociado
+            // 
+            panelAssociado.BorderStyle = BorderStyle.FixedSingle;
+            panelAssociado.Location = new Point(184, -1);
+            panelAssociado.Name = "panelAssociado";
+            panelAssociado.Size = new Size(615, 449);
+            panelAssociado.TabIndex = 11;
+            // 
+            // button4
+            // 
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(-1, 404);
+            button4.Name = "button4";
+            button4.Size = new Size(186, 44);
+            button4.TabIndex = 10;
+            button4.Text = "Sair";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(-1, 227);
+            button3.Name = "button3";
+            button3.Size = new Size(186, 45);
+            button3.TabIndex = 9;
+            button3.Text = "Deletar";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(-1, 176);
+            button2.Name = "button2";
+            button2.Size = new Size(186, 45);
+            button2.TabIndex = 8;
+            button2.Text = "Cadastrar";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(-1, 125);
+            button1.Margin = new Padding(0);
+            button1.Name = "button1";
+            button1.Size = new Size(186, 48);
+            button1.TabIndex = 7;
+            button1.Text = "Consultar";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(224, 224, 224);
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(-1, -1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(186, 123);
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // frmPageAssociado
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panelAssociado);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "frmPageAssociado";
+            Text = "frmCadastrarAssociado";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelAssociado;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ frmCadastrarAssociado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Emprestimo___Biblioteca
+{
+    public partial class frmPageAssociado : Form
+    {
+        public frmPageAssociado()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmCadastraAssociado frmCadastraAssociado = new frmCadastraAssociado();
+            frmCadastraAssociado.TopLevel = false;
+            frmCadastraAssociado.FormBorderStyle = FormBorderStyle.None;
+            frmCadastraAssociado.Dock = DockStyle.Fill;
+            panelAssociado.Controls.Clear();
+            panelAssociado.Controls.Add(frmCadastraAssociado);
+            frmCadastraAssociado.Show();
+        }
+    }
+}
