d7da4a1aa2d32cbc5d2457d3eeb9d23cb391fab2
adriano <adrianobinan@gmail.com>
2024-12-04 16:14:19 -0300

    alterações

--- obj/Release/net8.0-windows/Sistema de Emprestimo - Biblioteca.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/Sistema de Emprestimo - Biblioteca.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema de Emprestimo - Biblioteca")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db604b1dea332c6cc754e37a050daba3b5c19438")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema de Emprestimo - Biblioteca")]
